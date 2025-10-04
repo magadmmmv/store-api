9bc44a7b46ebbf92d0d5ad67706cb9fb9dd4abf3
Magad Mammaev <magadmmmv@gmail.com>
2025-10-04 23:31:18 +0300

    003 Добавлено подключение к postgres и контроллер

---
+++ Api/Controller/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controller
+{
+    public class ProductController : StoreController
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            return Ok(await Task.FromResult("hello world"));
+        }
+    }
+}
---
+++ Api/Controller/StoreController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controller
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+
+    }
+}
