e3095c0799b4ecd32c9951d71a3f9ae7f8138f1c
Magad Mammaev <magadmmmv@gmail.com>
2025-10-04 05:23:34 +0300

    002 Добавлен swagger

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/hello", () => "hello store api");
 
