e034ec65b7d696a2110106c8e5db4ca2568a2d39
Magad Mammaev <magadmmmv@gmail.com>
2025-10-06 01:05:57 +0300

    004 добавлена миграция ef

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,12 @@
+using Api.Extension;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddPostgreSqlDbContext(builder.Configuration);
+
 
 var app = builder.Build();
 app.MapControllers();
