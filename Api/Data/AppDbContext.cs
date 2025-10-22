6b5c124fefc7565d6d41baaf755c1e5e6003489d
Magad Mammaev <MMammaev@it-one.ru>
2025-10-23 00:56:30 +0300

    Добавлены фейковые данные в таблицу Product

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Api.Model;
+using Api.Seed;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace Api.Data
 {
@@ -12,5 +14,18 @@ namespace Api.Data
         }
 
         public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Product>().HasData(FakeProductGenerator.GenerateProductList());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.ConfigureWarnings(warnings =>
+              warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        }
     }
 }
--- Api/Migrations/20251022215028_SeedData.cs
+++ Api/Migrations/20251022215028_SeedData.cs
@@ -4,10 +4,12 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace Api.Migrations
 {
     /// <inheritdoc />
-    public partial class AddDbContext : Migration
+    public partial class SeedData : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -31,6 +33,8 @@ namespace Api.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "text", nullable: false),
+                    Discriminator = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: true),
                     UserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
                     Email = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
@@ -49,6 +53,24 @@ namespace Api.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    SpecialTag = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
@@ -157,6 +179,33 @@ namespace Api.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "Image", "Name", "Price", "SpecialTag" },
+                values: new object[,]
+                {
+                    { 1, "Категория 2", "Активности эксперимент намеченных следует кадровой активом этих активизации.", "https://placehold.co/200", "Великолепный Хлопковый Свитер", 439.18000000000001, "Новинка" },
+                    { 2, "Категория 3", "Форм вызывает поэтапного.", "https://placehold.co/200", "Маленький Неодимовый Стол", 666.63999999999999, "Популярный" },
+                    { 3, "Категория 1", "Степени анализа модели.", "https://placehold.co/200", "Свободный Неодимовый Стол", 191.50999999999999, "Популярный" },
+                    { 4, "Категория 1", "Образом широкому дальнейшее начало мира.", "https://placehold.co/200", "Интеллектуальный Хлопковый Портмоне", 327.62, "Рекомендуемый" },
+                    { 5, "Категория 3", "Высшего создание модернизации определения дальнейших широкому значительной.", "https://placehold.co/200", "Свободный Хлопковый Ремень", 30.34, "Рекомендуемый" },
+                    { 6, "Категория 3", "Нами однако форм современного качества предпосылки.", "https://placehold.co/200", "Лоснящийся Натуральный Кепка", 417.79000000000002, "Новинка" },
+                    { 7, "Категория 3", "Активизации обществом уточнения понимание забывать.", "https://placehold.co/200", "Большой Натуральный Сабо", 457.06999999999999, "Новинка" },
+                    { 8, "Категория 2", "Демократической реализация стороны новая нами направлений рамки анализа.", "https://placehold.co/200", "Большой Натуральный Кепка", 214.16, "Рекомендуемый" },
+                    { 9, "Категория 1", "Богатый оценить определения курс.", "https://placehold.co/200", "Великолепный Хлопковый Кепка", 126.16, "Популярный" },
+                    { 10, "Категория 1", "Укрепления важные обеспечивает предложений порядка требует постоянный.", "https://placehold.co/200", "Интеллектуальный Натуральный Шарф", 286.81, "Новинка" },
+                    { 11, "Категория 3", "Равным роль насущным анализа обучения обучения значение важные за.", "https://placehold.co/200", "Грубый Хлопковый Берет", 176.65000000000001, "Рекомендуемый" },
+                    { 12, "Категория 2", "Консультация от изменений сложившаяся организационной уровня же.", "https://placehold.co/200", "Интеллектуальный Кожанный Компьютер", 926.79999999999995, "Популярный" },
+                    { 13, "Категория 3", "Работы понимание проверки дальнейших материально-технической настолько задания следует.", "https://placehold.co/200", "Фантастический Гранитный Автомобиль", 429.55000000000001, "Рекомендуемый" },
+                    { 14, "Категория 2", "Разработке массового следует административных повышение.", "https://placehold.co/200", "Великолепный Бетонный Ножницы", 65.209999999999994, "Рекомендуемый" },
+                    { 15, "Категория 3", "Идейные создание финансовых создание инновационный проект требует создаёт соображения организации.", "https://placehold.co/200", "Маленький Деревянный Кепка", 231.78999999999999, "Рекомендуемый" },
+                    { 16, "Категория 3", "Опыт роль общественной.", "https://placehold.co/200", "Великолепный Меховой Шарф", 16.32, "Рекомендуемый" },
+                    { 17, "Категория 1", "Определения важную систему дальнейшее модели постоянное направлений образом от.", "https://placehold.co/200", "Большой Бетонный Шарф", 689.76999999999998, "Популярный" },
+                    { 18, "Категория 2", "Активом важные однако.", "https://placehold.co/200", "Лоснящийся Натуральный Кепка", 997.95000000000005, "Новинка" },
+                    { 19, "Категория 3", "Широкому модель уровня за создаёт целесообразности потребностям активизации.", "https://placehold.co/200", "Невероятный Пластиковый Плащ", 633.25, "Рекомендуемый" },
+                    { 20, "Категория 2", "Принимаемых целесообразности ресурсосберегающих постоянный формировании же модернизации.", "https://placehold.co/200", "Грубый Неодимовый Сабо", 938.49000000000001, "Рекомендуемый" }
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_AspNetRoleClaims_RoleId",
                 table: "AspNetRoleClaims",
@@ -212,6 +261,9 @@ namespace Api.Migrations
 
             migrationBuilder.DropTable(
                 name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Products");
 
             migrationBuilder.DropTable(
                 name: "AspNetRoles");
