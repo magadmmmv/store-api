8f116d6b58df4436dff3291d9c88ce03608b80f0
Magad Mammaev <MMammaev@it-one.ru>
2025-11-22 14:22:31 +0300

    013 добавлен сервис генерации JWT-токенов

---
+++ Api/Service/JwtTokenGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Api.Model;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+
+namespace Api.Service
+{
+    public class JwtTokenGenerator
+    {
+        private readonly string secretKey;
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            this.secretKey = configuration["AuthSettings:SecretKey"];
+        }
+
+        public string GenerateJwtToken(AppUser appUser, IList<string> roles)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("FirstName", appUser.FirstName),
+                    new Claim("id", appUser.Id),
+                    new Claim(ClaimTypes.Email, appUser.Email),
+                    new Claim(ClaimTypes.Role, String.Join(",", roles))
+                }),
+
+                Expires = DateTime.UtcNow.AddDays(1),
+
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature
+                )
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
