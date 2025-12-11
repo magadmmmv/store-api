29e19468aed6e737168284b8abc15e266eede4a9
Magad Mammaev <MMammaev@it-one.ru>
2025-12-11 09:26:59 +0300

    027 добавлен контроллер проведения фейковой оплаты

---
+++ Api/Controller/PaymentController.cs
@@ -0,0 +1,28 @@
+﻿using Api.Data;
+using Api.Model;
+using Api.Service.Payment;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.Controller
+{
+    public class PaymentController : StoreController
+    {
+        private readonly IPaymentService paymentService;
+
+        public PaymentController(AppDbContext dbContext,
+            IPaymentService paymentService) 
+            : base(dbContext)
+        {
+            this.paymentService = paymentService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ResponseServer>> MakePayment(
+            string userId, int orderHeaderId, string cardNumber)
+        {
+            return await paymentService
+                .HandlePaymentAsync(userId, orderHeaderId, cardNumber);
+        }
+    }
+}
